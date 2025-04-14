f293e976058123bfe650b4dec79ba02fb03b5e7b
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-14 21:19:14 +0100

    added query response on form search

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,7 @@ router.get("/", (req, res) => res.render("index.ejs", {title: "Dynamic Title", h
 router.get("/about", (req, res) => res.render("about.ejs", {aboutTitle: "About", aboutHeader: "This is the about page"}));
 router.get("/contact", (req, res) => res.render("contact.ejs"))
 router.get("/search", (req, res) => res.render("search.ejs"))
+router.get("/search_result", (req, res) => res.send("You searched for " + req.query.search_text + " " + req.query.category ))
 
 // Export the router object
 module.exports = router
